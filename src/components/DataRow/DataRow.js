8ae05d84b34b9f6f875caacf095cce886ca63734
Greg Woodring <gregwoodring@Gregs-Air.attlocal.net>
2022-02-28 21:33:53 -0500

    quick and dirty table rendering

--- src/components/DataRow/DataRow.js
+++ src/components/DataRow/DataRow.js
@@ -1,7 +1,22 @@
 import React from 'react'
-
 const DataRow = props => {
-
+    return (
+        <div className='data-row'>
+            <div>{props.ward}</div>
+            <div>{props.dem}</div>
+            <div>{props.rep}</div>
+            <div>{props.other_party}</div>
+            <div>{props.white}</div>
+            <div>{props.black}</div>
+            <div>{props.hispanic}</div>
+            <div>{props.other_race}</div>
+            <div>{props.male}</div>
+            <div>{props.female}</div>
+            <div>{props.unknown_sex}</div>
+            <div>{props.total}</div>
+            <div>{'placeholder'}</div>
+        </div>
+    )
 }
 
 export default DataRow;
--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import {connect} from 'react-redux';
-
+import DataRow from '../DataRow/DataRow';
+import './DataTable.scss'
 const DataTable = props => {
     return (
-        <div>
-
+        <div className='data-table'>
+            <div className='table-header data-row'>
+                <div>Ward</div>
+                <div>Democrat</div>
+                <div>Republican</div>
+                <div>Other Party</div>
+                <div>White</div>
+                <div>Black</div>
+                <div>Hispanic</div>
+                <div>Other Race</div>
+                <div>Male</div>
+                <div>Female</div>
+                <div>Unknown Sex</div>
+                <div>Total</div>
+                <div>%</div>
+            </div>
+            {props.voterArray.map((item, index) => <DataRow 
+                ward={item.ward}
+                dem={item.dem}
+                rep={item.rep}
+                other_party={item.other_party}
+                total={item.total}
+                white={item.white}
+                black={item.black}
+                hispanic={item.hispanic}
+                other_race={item.other_race}
+                male={item.male}
+                female={item.female}
+                unknown_sex={item.unknown_sex}
+                key={index} 
+            />)}
         </div>
     )
 }
