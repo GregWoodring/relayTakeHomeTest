fbe93830858fee902df24410f6dd28a07c3ba0cc
Greg Woodring <gregwoodring@Gregs-Air.attlocal.net>
2022-02-28 22:05:44 -0500

    added logic to update percent column

--- src/components/DataRow/DataRow.js
+++ src/components/DataRow/DataRow.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {connect} from 'react-redux';
+
 const DataRow = props => {
     return (
         <div className='data-row'>
@@ -14,9 +16,15 @@ const DataRow = props => {
             <div>{props.female}</div>
             <div>{props.unknown_sex}</div>
             <div>{props.total}</div>
-            <div>{'placeholder'}</div>
+            <div>{(Math.floor((props[props.selectedSegmentName]/props.total)*100)/100)}</div>
         </div>
     )
 }
 
-export default DataRow;
+function mapStateToProps(state) {
+    return {
+        selectedSegmentName: state.voters.selectedSegmentName
+    }
+}
+
+export default connect(mapStateToProps)(DataRow);
